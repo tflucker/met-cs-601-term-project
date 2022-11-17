003a77cb89b5efe9c85bd0f1e9281667ea63d8e0
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-11-17 11:58:51 -0500

    adding comments, add style to not found page

--- term-project/src/pages/NotFound.vue
+++ term-project/src/pages/NotFound.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>404 - Page Not Found</h1>
-    <p>No Vue file is defined for this link. Its intentional, and it shows that this application can handle an invalid
-        URL gracefully!</p>
+    <main id="main-content">
+        <section class="page-jumbotron sea-green text-center">
+        <h2>404 - Page Not Found</h2>
+        <p>No Vue file is defined for this link. Its intentional, and it shows that this application can handle an
+            invalid
+            URL gracefully!</p>
+    </section>
+    </main>
 </template>
