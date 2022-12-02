96852eda0b8205c65989d6e6bb2e04c4cfe15f8a
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-12-01 19:42:57 -0500

    added header, summary, new functionality

--- json-webserver/main.js
+++ json-webserver/main.js
@@ -1,34 +1,42 @@
-// (function () {
-//     document.getElementById("fetchDataBtn").onclick = function() {
-//         makeRequest("https://pokeapi.co/api/v2/pokemon/");
-//     };
+function makeRequest(url, type){
 
-//     function makeRequest(url){
-//         fetch(url).then((response) => {
-//             document.getElementById("responseViewer").innerHTML = response.json;
-//         }).then((data))
-//         .catch((error) => {
-//             document.getElementById("responseViewer").innerHTML = "An error has occurred. Please try again.";
-//             console.error(error);
-//         })
-//     }
-// })();
-
-function makeRequest(url){
     fetch(url).then((response) => {
         if(!response.ok){
-            return console.log("ERROR!");
+            document.getElementById("fetchStatus").innerHTML = "ERROR";
+            console.log("ERROR!");
+            throw new Error("There was an error during the Fetch request. Please try again.");
         } else {
             console.log("GOOD!")
+            document.getElementById("fetchStatus").innerHTML = "SUCCESS";
+            // returns json data to next 'then' clause. 
             return response.json();
         }
 
     }).then((json) => {
-        console.log(json);
-        document.getElementById("responseViewer").textContent = JSON.stringify(json, undefined, 2);
+        if(type == "findAll"){
+            console.log(json);
+            document.getElementById("responseViewer").textContent = JSON.stringify(json, undefined, 2);
+        } else if (type == "findByIndex"){
+            let id = document.getElementById("jsonIndex").value.trim();
+            let data = {};
+            if(id > json.my_education.length - 1){
+                data = {
+                    "code": 500,
+                    "message": "Invalid value provided.  Please provide values between 0 and " + new String(json.my_education.length - 1) + "(max index of data)."
+                }
+            } else {
+                data = json.my_education[id];
+            }
+            console.log(data);
+            document.getElementById("responseViewer").textContent = JSON.stringify(data, undefined, 2);
+        }
     })
     .catch((error) => {
-        document.getElementById("responseViewer").textContent = "An error has occurred. Please try again.";
-        console.error(error);
+        let errorObj = {
+            "code": 500,
+            "message": error.message
+        }
+        document.getElementById("responseViewer").textContent = JSON.stringify(errorObj, undefined, 2);
+        console.log(error.message);
     })
 }
