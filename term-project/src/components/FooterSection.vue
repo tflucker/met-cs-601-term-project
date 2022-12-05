85d637fbdc0d87aa6cdf96eb81cd661317ab7506
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-12-05 13:16:24 -0500

    removing br tags

--- term-project/src/components/FooterSection.vue
+++ term-project/src/components/FooterSection.vue
@@ -11,17 +11,17 @@ export default {
 <template>
   <!-- Footer giving author and version information -->
   <footer class="footer-style">
-    <a href="#page-top"
+    <a href="#page-top" class="margin-y-sm"
       ><img
         src="https://static.thenounproject.com/png/691751-200.png"
         class="back-to-top-icon"
         alt="Back to Top icon by Noun Project"
       />&nbsp;Back to Top</a
-    ><br />
-    <p class="text-center">
-      Created By: <strong>Timothy Flucker</strong><br />
-      Last Updated On: <strong>{{ lastUpdateDate }}</strong><br />
-      Version: <strong>{{ versionNum }}</strong>
-    </p>
+    >
+    <div class="text-center">
+      <p>Created By: <strong>Timothy Flucker</strong></p>
+      <p>Last Updated On: <strong>{{ lastUpdateDate }}</strong></p>
+      <p>Version: <strong>{{ versionNum }}</strong></p>
+    </div>
   </footer>
 </template>
